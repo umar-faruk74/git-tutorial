955614e64d558446db8b1b5ee6670017d0fbaddd
umar faruk <infofaruk74@gmail.com>
2020-11-23 11:52:28 +0600

    added another function

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ function color(fav1, fav2){
     console.log(`${fav1} is my favourite color. ${fav2} is favourite as well.`);
 }
 color('black', 'yellow');
+
+let actor = function(act1, act2){
+    console.log(`${act1} and ${act2} are my two favourite actors.`);
+}
+ actor('Leo', 'Brad');
+ 
